d734060d5fae41c83cef876aa9780337971dbde9
Parag Chandra <43547001+cparag@users.noreply.github.com>
2025-01-13 14:26:44 -0600

    Update Face Recognition_Project.py

--- Face Recognition_Project.py
+++ Face Recognition_Project.py
@@ -3,7 +3,7 @@ import numpy as np
 import face_recognition
 import os
 from datetime import datetime
-# from PIL import ImageGrab
+## from PIL import ImageGrab
  
 path = 'ImagesAttendance'
 images = []
